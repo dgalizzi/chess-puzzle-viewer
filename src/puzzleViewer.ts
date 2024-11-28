5e3fdca672b4cc87cf907708d139e72cb955f9b5
Diego Daniel Galizzi <dgalizzi@gmail.com>
2024-11-28 10:11:48 -0300

    Refactored handle move logic into smaller and more readable functions

--- src/puzzleViewer.ts
+++ src/puzzleViewer.ts
@@ -3,7 +3,14 @@ import { Chess } from "chessops/chess";
 import { Config } from "chessground/config";
 import { makeFen } from "chessops/fen";
 import { chessgroundDests } from "chessops/compat";
-import { Color, defaultSetup, parseSquare, parseUci, Role } from "chessops";
+import {
+  Color,
+  defaultSetup,
+  Move,
+  parseSquare,
+  parseUci,
+  Role,
+} from "chessops";
 import { Key, Piece } from "chessground/types";
 
 export default class PuzzleViewer {
@@ -13,13 +20,6 @@ export default class PuzzleViewer {
   promotion: { dest: any; color: any; resolve: any } | null;
 
   constructor(readonly redraw: () => void) {
-    // r1bqkbnr/pPn1pppp/8/8/8/8/PPPP1PPP/RNBQKBNR w KQkq - 1 5
-    //
-    // const setup = parseFen(
-    //   "r1bqkbnr/pPn1pppp/8/8/8/8/PPPP1PPP/RNBQKBNR w KQkq - 1 5",
-    // ).unwrap();
-    // this.pos = Chess.fromSetup(setup).unwrap();
-
     this.pos = Chess.fromSetup(defaultSetup()).unwrap();
     this.promotion = null;
   }
@@ -83,63 +83,80 @@ export default class PuzzleViewer {
         dests: chessgroundDests(pv.pos),
         events: {
           after(orig, dest, metadata) {
-            const uciMove = `${orig}${dest}`;
-            const move = parseUci(uciMove);
-            if (!move) return undefined;
-
-            const piece = pv.ground!.state.pieces.get(dest);
-            if (!piece) {
-              return undefined;
-            }
-
-            // Handle promotion
-            if (pv.isPromotion(dest, piece)) {
-              if ("promotion" in move) {
-                pv.promptPromotion(dest, piece.color).then(
-                  (role: Role | undefined) => {
-                    if (role !== undefined) {
-                      move.promotion = role;
-                      piece.role = role;
-                      piece.promoted = true;
-                      pv.ground!.setPieces(new Map([[dest, piece]]));
-                      pv.pos.play(move);
-                    } else {
-                      // User clicked outside of any of the promotion pieces
-                      // Set the previous position
-                      pv.ground?.set({ fen: makeFen(pv.pos.toSetup()) });
-                    }
-                    pv.ground!.set(pv.cgState());
-                    pv.promotion = null;
-                  },
-                );
-                return undefined;
-              }
-            }
-
-            // Handle en passant
-            const enPassantSquareCapture = pv.getEnPassantCaptureSquare(
-              orig,
-              dest,
-              (s: string) => pv.pos.board.get(parseSquare(s)!),
-            );
-
-            if (enPassantSquareCapture) {
-              // Remove the captured piece
-              pv.ground!.setPieces(
-                new Map([[enPassantSquareCapture as Key, undefined]]),
-              );
-            }
-
-            // Make the move
-            pv.pos.play(move);
-
-            // Update legal moves
-            pv.ground!.set({ movable: { dests: chessgroundDests(pv.pos) } });
-            // Alternatively update whole config?
-            // pv.ground!.set(pv.cgState());
+            pv.handleMove(orig, dest);
           },
         },
       },
     };
+  }
+
+  private handleMove(orig: Key, dest: Key): void {
+    const uciMove = `${orig}${dest}`;
+    const move = parseUci(uciMove);
+    if (!move) return;
+
+    const piece = this.ground!.state.pieces.get(dest);
+    if (!piece) return;
+
+    if (this.isPromotion(dest, piece)) {
+      this.handlePromotion(dest, piece, move);
+      return;
+    }
+
+    this.handleEnPassant(orig, dest);
+    this.makeMove(move);
+  }
+
+  private handlePromotion(dest: Key, piece: Piece, move: Move): void {
+    this.promptPromotion(dest, piece.color).then((role: Role | undefined) => {
+      if (role !== undefined) {
+        this.applyPromotion(dest, piece, role, move);
+      } else {
+        this.resetToPreviousPosition();
+      }
+      this.updateLegalMoves();
+    });
+  }
+
+  private applyPromotion(
+    dest: Key,
+    piece: Piece,
+    role: Role,
+    move: Move,
+  ): void {
+    if ("promotion" in move) {
+      move.promotion = role;
+      piece.role = role;
+      piece.promoted = true;
+      this.ground!.setPieces(new Map([[dest, piece]]));
+      this.pos.play(move);
+    }
+  }
+
+  private resetToPreviousPosition(): void {
+    this.ground?.set({ fen: makeFen(this.pos.toSetup()) });
+  }
+
+  private handleEnPassant(orig: string, dest: string): void {
+    const enPassantSquareCapture = this.getEnPassantCaptureSquare(
+      orig,
+      dest,
+      (s: string) => this.pos.board.get(parseSquare(s)!),
+    );
+
+    if (enPassantSquareCapture) {
+      this.ground!.setPieces(
+        new Map([[enPassantSquareCapture as Key, undefined]]),
+      );
+    }
+  }
+
+  private makeMove(move: Move): void {
+    this.pos.play(move);
+    this.updateLegalMoves();
+  }
+
+  private updateLegalMoves(): void {
+    this.ground!.set({ movable: { dests: chessgroundDests(this.pos) } });
   }
 }
